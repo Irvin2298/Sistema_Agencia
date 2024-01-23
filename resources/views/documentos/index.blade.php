bb30fd76d3bf2af18e163e3b8f511529062a925f
Irvin2298 <gonzalescruzjuan33@gmail.com>
2024-01-22 18:01:57 -0600

    Cargando div y buscador en el select

--- resources/views/documentos/index.blade.php
+++ resources/views/documentos/index.blade.php
@@ -27,15 +27,15 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="sexo">Selecciona el documento a generar: </label>
-                                        <select required name="documento" class="form-control" id="documentos" onchange="cargarContenido()">
+                                        <select required name="documento" class="form-control selectpicker" data-live-search="true" id="documentos" onchange="cargarContenido()">
                                             <option disabled selected value="">Documentos</option>
                                             <option value="Recibo">Recibo</option>
-                                            <!-- <option value="Femenino">Femenino</option> -->
+                                            <option value="Nombramiento">Nombramiento</option>
                                         </select>
                                     </div>
                                 </div>
                             </div>
-                        <div id="contenidoRecibo"> 
+                        <div id="contenidoRecibo" class="opcionesDiv" style="display: none;"> 
                         <form action="{{ route('documentos.crearRecibo') }}" method="post"  target="_blank">
                             @csrf
                             <!-- Datos para generar el recibo -->
@@ -45,6 +45,66 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="nombre">Nombre: </label><span class="required text-danger">*</span>
+                                        <input type="text" name="nombre" pattern="[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+" title="Solo se permiten letras y espacios" class="form-control" placeholder="Nombre/s" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="apellido_p">Apellido paterno: </label><span class="required text-danger">*</span>
+                                        <input type="text" name="apellido_p" pattern="[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+"  title="Solo se permiten letras" class="form-control" placeholder="Apellido Paterno" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="apellido_m">Apellido Materno: </label><span class="required text-danger">*</span>
+                                        <input type="text" name="apellido_m" pattern="[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+"  title="Solo se permiten letras" class="form-control" placeholder="Apellido Materno" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="cantidad_numero">Cantidad: </label><span class="required text-danger">*</span>
+                                        <input type="number" name="cantidad_numero"  title="Solo se permiten números" class="form-control" placeholder="Cantidad del recibo en número" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="cantidad_letra">Cantidad: </label><span class="required text-danger">*</span>
+                                        <input type="text" name="cantidad_letra" pattern="[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+"  title="Solo se permiten letras" class="form-control" placeholder="Cantidad del recibo en letra" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-group nf-date">
+                                        <label for="fecha_recibo">Fecha: <span class="required text-danger">*</span></label>
+                                        <input required type="date" name="fecha_recibo" max="{{ now()->format('Y-m-d') }}" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="concepto_recibo">Concepto: <span class="required text-danger">*</span></label>
+                                        <textarea class="form-control" id="concepto_recibo" name="concepto_recibo" style="height: 100px;" placeholder="Escribe el concepto del recibo"></textarea>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary" style="float: right;" target="_blank">Generar Recibo</button>
+                                </div>
+                            </div>
+                            
+                        </form>
+                        </div>
+                            <!-- Terminan los datos para generar el recibo -->
+                            <div id="contenidoNombramiento" class="opcionesDiv" style="display: none;"> 
+                        <form action="{{ route('documentos.crearRecibo') }}" method="post"  target="_blank">
+                            @csrf
+                            <!-- Datos para generar el nombramiento -->
+                            
+                            <label class="text-danger">Los campos con * son obligatorios</label>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="nombre">Nombre nombraiento: </label><span class="required text-danger">*</span>
                                         <input type="text" name="nombre" pattern="[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+" title="Solo se permiten letras y espacios" class="form-control" placeholder="Nombre/s" required>
                                     </div>
                                 </div>
@@ -115,8 +175,8 @@
                 case "Recibo":
                     contenidoRecibo.style.display = "block";
                     break;
-                case "opcion2":
-                    contenidoDiv.innerHTML = "<p>Contenido para la Opción 2</p>";
+                case "Nombramiento":
+                    contenidoNombramiento.style.display = "block";
                     break;
                 case "opcion3":
                     contenidoDiv.innerHTML = "<p>Contenido para la Opción 3</p>";
@@ -126,4 +186,24 @@
             }
         }
     </script>
+
+<script>
+    $(document).ready(function () {
+        $('.selectpicker').selectpicker();
+    });
+</script>
+
+<script>
+    $(document).ready(function () {
+        // Manejar el evento de cambio en el select
+        $('#documentos').change(function () {
+            // Ocultar todos los divs
+            $('.opcionesDiv').hide();
+
+            // Mostrar el div correspondiente a la opción seleccionada
+            var opcionSeleccionada = $(this).val();
+            $('#contenido' + opcionSeleccionada).show();
+        });
+    });
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,19 @@
     <link  href="http://fonts.googleapis.com/css?family=Reenie+Beanie:regular" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/post-it.css') }}" rel="stylesheet" type="text/css"/>
 
+    <!-- Incluir jQuery desde CDN -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<!-- Incluir Bootstrap desde CDN (Requiere Popper.js) -->
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+<!-- <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script> -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<!-- Incluir selectpicker desde CDN -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+
 @yield('page_css')
 <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('web/css/style.css') }}">
